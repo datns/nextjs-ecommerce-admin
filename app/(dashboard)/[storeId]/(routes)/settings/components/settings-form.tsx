c3e487bd3a7afc36c838cbeec4054d5e1f8b916b
dat.s.nguyen <dat.s.snguyen@taptap.com.vn>
2023-09-15 09:59:33 +0700

    build api alert

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -15,6 +15,8 @@ import {toast} from "react-hot-toast";
 import axios from "axios";
 import {useParams, useRouter} from "next/navigation";
 import {AlertModal} from "@/components/modals/alert-modal";
+import {ApiAlert} from "@/components/ui/api-alert";
+import {useOrigin} from "@/hooks/use-origin";
 
 interface SettingsFormProps {
 	initialData: Store;
@@ -37,7 +39,8 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
 		defaultValues: initialData
 	})
 	const params = useParams();
-	const router  = useRouter();
+	const router = useRouter();
+	const origin = useOrigin();
 
 	const onSubmit = async (values: SettingsFormValues) => {
 		try {
@@ -73,7 +76,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
 				isOpen={open}
 				onClose={() => setOpen(false)}
 				onConfirm={onDelete}
-				loading={loading} />
+				loading={loading}/>
 			<div className="flex items-center justify-between">
 				<Heading
 					title="Settings"
@@ -89,7 +92,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
 					<Trash className="h-4 w-4"/>
 				</Button>
 			</div>
-			<Separator />
+			<Separator/>
 			<Form {...form}>
 				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
 					<div className="grid grid-cols-3 gap-8">
@@ -108,7 +111,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
 											{...field}
 										/>
 									</FormControl>
-									<FormMessage />
+									<FormMessage/>
 								</FormItem>
 							)}
 						/>
@@ -118,7 +121,12 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
 					</Button>
 				</form>
 			</Form>
+			<Separator/>
+			<ApiAlert
+				title="NEXT_PUBLIC_API_URL"
+				description={`${origin}/api/${params.storeId}`}
+				variant="public"
+			/>
 		</>
-
 	)
 }
