3447b8afcd5ebd2014bd24ca777164ee79042620
dat.s.nguyen <dat.s.snguyen@taptap.com.vn>
2023-09-18 11:14:39 +0700

    build billboards page

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -5,21 +5,30 @@ import {Button} from "@/components/ui/button";
 import {Plus} from "lucide-react";
 import {Separator} from "@/components/ui/separator";
 import {useParams, useRouter} from "next/navigation";
+import {BillboardColumn, columns} from "@/app/(dashboard)/[storeId]/(routes)/billboards/components/columns";
+import {DataTable} from "@/components/ui/data-table";
 
-export const BillboardClient = () => {
+interface BillboardClientProps {
+	data: BillboardColumn[]
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+	data
+																}) => {
 	const router = useRouter();
 	const params = useParams();
 
 	return (
 		<>
 			<div className="flex items-center justify-between">
-				<Heading title="Billboards (0)" description="Manage billboards for your store" />
+				<Heading title={`Billboards (${data.length})`} description="Manage billboards for your store" />
 				<Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
 					<Plus className="mr-2 h-4 w-4" />
 					Add New
 				</Button>
 			</div>
 			<Separator />
+			<DataTable columns={columns} data={data} searchKey="label" />
 		</>
 	)
 }
