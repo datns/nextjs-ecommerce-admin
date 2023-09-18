5693b9fa7d8f25fd6a08203f93d3040ef37c1f3d
dat.s.nguyen <dat.s.snguyen@taptap.com.vn>
2023-09-18 15:04:07 +0700

    handle CRUD size

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -0,0 +1,35 @@
+import {SizeClient} from "./components/client";
+import prismadb from "@/lib/prismadb";
+import {SizeColumn} from "./components/columns";
+import format from 'date-fns/format'
+
+const SizesPage = async ({
+	params
+							  }: {
+	params: { storeId: string }
+}) => {
+	const sizes = await prismadb.size.findMany({
+		where: {
+			storeId: params.storeId
+		},
+		orderBy: {
+			createdAt: 'desc'
+		}
+	})
+
+	const formatSizes: SizeColumn[] = sizes.map((item) => ({
+		id: item.id,
+		name: item.name,
+		value: item.value,
+		createdAt: format(item.createdAt, 'MMMM do, yyyy')
+	}))
+	return (
+		<div className="flex-col">
+			<div className="flex-1 space-y-4 p-8 pt-6">
+				<SizeClient data={formatSizes} />
+			</div>
+		</div>
+	)
+}
+
+export default SizesPage;
