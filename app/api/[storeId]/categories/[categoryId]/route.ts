01d2b69de0773fe77a9c63b141c083f2008efb6c
dat.s.nguyen <dat.s.snguyen@taptap.com.vn>
2023-09-30 21:41:12 +0700

    handle checkout

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -8,11 +8,11 @@ export async function GET(
 	{ params } : { params: { categoryId: string }}
 ) {
 	try {
-		const {userId} = auth();
-
-		if (!userId) {
-			return new NextResponse("Unauthenticated", { status: 401 })
-		}
+		// const {userId} = auth();
+		//
+		// if (!userId) {
+		// 	return new NextResponse("Unauthenticated", { status: 401 })
+		// }
 
 		if (!params.categoryId) {
 			return new NextResponse("Category id is required", { status: 400 })
@@ -21,6 +21,9 @@ export async function GET(
 		const category = await prismadb.category.findUnique({
 			where: {
 				id: params.categoryId,
+			},
+			include: {
+				billboard: true
 			}
 		})
 
